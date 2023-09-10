e1c88d8dafaf62134813a7cabc7151d1921f438c
KZsenpaii <animersgamers355@gmail.com>
2023-09-10 08:15:02 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$server ="localhost";
+$username ="root";
+$password ="";
+$database ="Pmb_Mochamad_Fiqri_UAS";
+
+$conn = mysqli_connect("$server", "$username", "$password", "$database");
+
+
+?>
